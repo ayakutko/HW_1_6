9b2bfb0196c9c637abac61f52962c28c4a604da2
ayakutko <anastasiia.yakutko@gmail.com>
2017-07-16 17:12:06 +0300

    HW1

---
+++ tasks_1_6.py
@@ -0,0 +1,16 @@
+import math
+a = 2
+b = 5
+c = 7
+#1
+print("Задание №1", a + b * (c / 2))
+#2
+print("Задание №2", (a**2 + b**2) % 2)
+#3
+print("Задание №3", (a + b) / 12 * c % 4 + b)
+#4
+print("Задание №4", (a - b * c) / (a + b) % c)
+#5
+print("Задание №5", abs(a - b) / (a + b)**3 - math.cos(c))
+#6
+print("Задание №6", (math.log(1 + c) / - b) **4 + abs(a))
